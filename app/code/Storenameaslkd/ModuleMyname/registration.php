5c82197a49684af5b3e60bdb4bfabd1e8e11dd6e
c1ear <gromadyuk@gmail.com>
2019-04-16 10:52:38 +0300

    фыв

--- app/code/Storenameaslkd/ModuleMyname/registration.php
+++ app/code/Storenameaslkd/ModuleMyname/registration.php
@@ -3,7 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+яячсфыв
 use \Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Storenameaslkd_ModuleMyname', __DIR__);
