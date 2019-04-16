c4bfa9dd0e24d4e4c926879fd085e6f415bab2f2
c1ear <gromadyuk@gmail.com>
2019-04-16 10:33:28 +0300

    app code

---
+++ app/design/frontend/storename/themename/requirejs-config.js
@@ -0,0 +1,7 @@
+var config = {
+    map: {
+        '*': {
+            "slick": "//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js",
+        }
+    }
+};
---
+++ app/etc/env.php
@@ -0,0 +1,61 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'e03fa853744d5c836dffa763e05be03f'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'newuser',
+                'password' => 'password',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '71e_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '71e_'
+            ]
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Wed, 10 Apr 2019 08:44:39 +0000'
+    ]
+];
